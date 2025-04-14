141b676d974fc12da337848dbd0a064feb909baa
Atharv Dixit <atharvdixit06@gmail.com>
2025-04-14 11:19:53 +0530

    refactor SideNav component for improved readability and maintainability

--- PokeDex/src/components/SideNav.jsx
+++ PokeDex/src/components/SideNav.jsx
@@ -7,7 +7,7 @@ export default function SideNav(props){
     const [ searchValue, setSearchValue] = useState('')
 
     const filteredPokemon = first151Pokemon.filter((ele , eleIndex) => {
-        if (toString(getFullPokedexNumber(eleIndex)).includes(searchValue)) { return true }
+        if (getFullPokedexNumber(eleIndex).includes(searchValue)) { return true }
         if ( ele.toLowerCase().includes(searchValue.toLowerCase())) { return true }
 
         return false
@@ -18,16 +18,17 @@ export default function SideNav(props){
         <div className={'header'}>
             <h1 className='text-gradient'>PokéDex</h1>
         </div>
-        <input value={searchValue} onChange={(e) => {
+        <input placeholder='E.g. 001 or Bulba...' value={searchValue} onChange={(e) => {
             setSearchValue(e.target.value)
         }}/>
         {filteredPokemon.map((pokemon, pokemonIndex)=>{
+            const truePokedexNumber = first151Pokemon.indexOf(pokemon)
             return (
                 <button onClick={() => {
-                    setSelectedPokemon(pokemonIndex)
+                    setSelectedPokemon(truePokedexNumber)
                 }} 
                 key={pokemon} className={'nav-card '+(pokemonIndex === selectedPokemon ? 'nav-card-selected ': ' ')}>
-                    <p>{getFullPokedexNumber(first151Pokemon.indexOf(pokemon))}</p>
+                    <p>{getFullPokedexNumber(truePokedexNumber)}</p>
                     <p>{pokemon}</p>
                 </button>
             )
